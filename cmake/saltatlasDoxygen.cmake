280070d39dbb75cdec52dd09630e3a0021bfff94
Keita Iwabuchi <iwabuchi1@llnl.gov>
2023-03-23 15:54:49 -0700

    Show .cpp files in doxygen generated docs.

--- cmake/saltatlasDoxygen.cmake
+++ cmake/saltatlasDoxygen.cmake
@@ -11,11 +11,16 @@ function(saltatlasDoxygen)
     set(DOXYGEN_HTML_OUTPUT
             ${PROJECT_BINARY_DIR}/docs/html)
     set(DOXYGEN_EXAMPLE_PATH ${PROJECT_SOURCE_DIR}/examples)
+    set(DOXYGEN_EXAMPLE_RECURSIVE YES)
+    set(DOXYGEN_SOURCE_BROWSER YES)
+    set(DOXYGEN_EXCLUDE_PATTERNS ${PROJECT_SOURCE_DIR}/utility/*.py)
 
     # Add a target 'saltatlas_doxygen'
     # Run 'make saltatlas_doxygen' to generate the docs
     doxygen_add_docs(saltatlas_doxygen
             ${PROJECT_SOURCE_DIR}/include
+            ${PROJECT_SOURCE_DIR}/examples
+            ${PROJECT_SOURCE_DIR}/utility
             COMMENT "Generate documentation by Doxygen"
             )
 endfunction()
