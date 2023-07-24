4b4c53f0c4b1916673e11e5d4f4573ace7065758
Benjamin Priest (they/them) <bwpriester@gmail.com>
2023-07-24 14:23:48 -0700

    changed inner typedef names from *_t to *_type (#55)

--- include/saltatlas/dhnsw/dhnsw.hpp
+++ include/saltatlas/dhnsw/dhnsw.hpp
@@ -18,31 +18,32 @@ template <typename DistType, typename IndexType, typename Point,
           template <typename, typename, typename> class Partitioner>
 class dhnsw {
  public:
-  using dist_t        = DistType;
-  using index_t       = IndexType;
-  using point_t       = Point;
-  using partitioner_t = Partitioner<dist_t, index_t, point_t>;
+  using dist_type     = DistType;
+  using index_type    = IndexType;
+  using point_type    = Point;
+  using partitioner_type = Partitioner<dist_type, index_type, point_type>;
 
   dhnsw(int max_voronoi_rank, int num_cells,
-        hnswlib::SpaceInterface<dist_t> *space_ptr, ygm::comm *comm,
-        partitioner_t &p)
+        hnswlib::SpaceInterface<dist_type> *space_ptr, ygm::comm *comm,
+        partitioner_type &p)
       : m_comm(comm),
         m_index_impl(max_voronoi_rank, num_cells, space_ptr, comm, p),
         m_query_engine_impl(&m_index_impl){};
 
   template <template <typename, typename, typename...> class Container>
-  void partition_data(Container<index_t, point_t> &data,
+  void partition_data(Container<index_type, point_type> &data,
                       const uint32_t               num_partitions) {
     m_index_impl.partition_data(data, num_partitions);
   }
 
-  void partition_data(ygm::container::array<point_t, index_t> &data,
+  void partition_data(ygm::container::array<point_type, index_type> &data,
                       const uint32_t                           num_partitions) {
     m_index_impl.partition_data(data, num_partitions);
   }
 
-  void partition_data(ygm::container::bag<std::pair<index_t, point_t>> &data,
-                      const uint32_t num_partitions) {
+  void partition_data(
+      ygm::container::bag<std::pair<index_type, point_type>> &data,
+      const uint32_t num_partitions) {
     m_index_impl.partition_data(data, num_partitions);
   }
 
@@ -52,7 +53,8 @@ class dhnsw {
     m_index_impl.set_hnsw_params(p);
   }
 
-  void queue_data_point_insertion(const index_t pt_idx, const point_t &pt) {
+  void queue_data_point_insertion(
+      const index_type pt_idx, const point_type &pt) {
     m_index_impl.add_data_point_to_insertion_queue(pt_idx, pt);
   }
 
@@ -73,13 +75,13 @@ class dhnsw {
   }
 
   /*
-void set_seeds(const std::vector<point_t> &seed_features) {
+void set_seeds(const std::vector<point_type> &seed_features) {
 m_index_impl.store_seeds(seed_features);
 }
   */
 
   template <typename Callback, typename... Callback_Args>
-  void query(const point_t &query_pt, const int k, const int hops,
+  void query(const point_type &query_pt, const int k, const int hops,
              const int voronoi_rank, const int initial_queries, Callback c,
              const Callback_Args &...args) {
     m_query_engine_impl.query(query_pt, k, hops, voronoi_rank, initial_queries,
@@ -87,7 +89,7 @@ m_index_impl.store_seeds(seed_features);
   }
 
   template <typename Callback, typename... Callback_Args>
-  void query_with_features(const point_t &query_pt, const int k, const int hops,
+  void query_with_features(const point_type &query_pt, const int k, const int hops,
                            const int voronoi_rank, const int initial_queries,
                            Callback c, const Callback_Args &...args) {
     m_query_engine_impl.query_with_features(query_pt, k, hops, voronoi_rank,
@@ -105,8 +107,8 @@ m_index_impl.store_seeds(seed_features);
 
  private:
   ygm::comm                                                      *m_comm;
-  dhnsw_detail::dhnsw_impl<dist_t, index_t, point_t, Partitioner> m_index_impl;
-  dhnsw_detail::query_engine_impl<dist_t, index_t, point_t, Partitioner>
+  dhnsw_detail::dhnsw_impl<dist_type, index_type, point_type, Partitioner> m_index_impl;
+  dhnsw_detail::query_engine_impl<dist_type, index_type, point_type, Partitioner>
       m_query_engine_impl;
 };
 
