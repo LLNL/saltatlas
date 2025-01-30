7cd080df3a1f148764308208614733ddebde1811
Keita Iwabuchi <iwabuchi1@lln.gov>
2025-01-30 00:35:59 -0800

    (DNND) Add get_neighbors() in simple API

--- examples/dnnd_simple.cpp
+++ examples/dnnd_simple.cpp
@@ -75,7 +75,9 @@ int main(int argc, char** argv) {
     }
 
     // Get nearest neighbours with features
-    comm.cout0()<< "\nNearest neighbor query result with features\n(show only the nearest point of the query from rank 0)" << std::endl;
+    comm.cout0() << "\nNearest neighbor query result with features\n(show only "
+                    "the nearest point of the query from rank 0)"
+                 << std::endl;
     {
       int        num_to_search = 4;
       const auto results =
@@ -85,7 +87,7 @@ int main(int argc, char** argv) {
         auto& neighbours = results.first;
         auto& features   = results.second;
         std::cout << "Point ID " << neighbours[0][0].id << ", distance "
-                     << neighbours[0][0].distance << ", feature {";
+                  << neighbours[0][0].distance << ", feature {";
         for (const auto& v : features[0][0]) {
           std::cout << v << " ";
         }
@@ -118,8 +120,9 @@ int main(int argc, char** argv) {
     }
   }
 
-  comm.cout0() << "\nGet points including the ones that are stored in other ranks"
-               << std::endl;
+  comm.cout0()
+      << "\nGet points including the ones that are stored in other ranks"
+      << std::endl;
   {
     id_t ids[]  = {0, 1};
     auto points = g.get_points(ids, ids + 2);
@@ -135,6 +138,19 @@ int main(int argc, char** argv) {
   // Dump a KNNG to files
   g.dump_graph("./knng");
   comm.cout0() << "KNNG dumped to ./knng" << std::endl;
+
+  comm.cout0() << "\nGet the neighbors" << std::endl;
+  {
+    id_t ids[]     = {0, 1};
+    const auto neighbors = g.get_neighbors(ids, ids + 2);
+    for (const auto& [id, nbs] : neighbors) {
+      comm.cout0() << "Point ID " << id;
+      for (const auto& nb : nbs) {
+        comm.cout0() << " -> " << nb;
+      }
+      comm.cout0() << std::endl;
+    }
+  }
 
   return 0;
 }
--- include/saltatlas/dnnd/dnnd_simple.hpp
+++ include/saltatlas/dnnd/dnnd_simple.hpp
@@ -310,6 +310,8 @@ class dnnd {
     return query_result;
   }
 
+  /// \brief Run queries as the same as query() and return the query results
+  /// with the feature vectors of the neighbors.
   template <typename query_iterator>
   std::pair<neighbor_store_type, std::vector<std::vector<point_type>>>
   query_with_features(query_iterator queries_begin, query_iterator queries_end,
@@ -369,28 +371,13 @@ class dnnd {
     return priv_get_point_partitioner()(id);
   }
 
-  /// \brief Get a point with the given ID from the local point store.
+  /// \brief Get a point of the given ID from the local point store.
   const point_type& get_local_point(const id_type id) const {
     return m_pstore.at(id);
   }
 
-  template <typename id_iterator>
-  std::unordered_map<id_type, point_type> get_local_points(
-      id_iterator ids_begin, id_iterator ids_end) const {
-    static_assert(
-        std::is_same_v<typename std::iterator_traits<id_iterator>::value_type,
-                       id_type>,
-        "id_iterator must be an iterator of id_type");
-
-    std::unordered_map<id_type, point_type> points;
-    points.reserve(std::distance(ids_begin, ids_end));
-    for (auto it = ids_begin; it != ids_end; ++it) {
-      const auto id = *it;
-      points.emplace(id, m_pstore.at(id));
-    }
-    return points;
-  }
-
+  /// \brief Get point data of the given IDs.
+  /// This function invokes YGM barrier. All ranks must call this function.
   template <typename id_iterator>
   std::unordered_map<id_type, point_type> get_points(
       id_iterator ids_begin, id_iterator ids_end) const {
@@ -404,6 +391,7 @@ class dnnd {
 
     static auto& ref_points = points;
     ref_points              = points;
+    m_comm.cf_barrier();
 
     auto proc = [](auto comm, auto pthis, const id_type id,
                    const int source_rank) {
@@ -447,6 +435,69 @@ class dnnd {
   /// This function performs an all-reduce operation, which is not cheap.
   std::size_t num_points() const {
     return m_comm.all_reduce_sum(m_pstore.size());
+  }
+
+  /// \brief Get the number of neighbors of the given point.
+  /// If the point is not stored locally, the function returns 0.
+  /// \param id Point ID.
+  /// \return The number of neighbors of the point.
+  std::size_t num_local_neighbors(const id_type id) const {
+    if (contains_local(id)) {
+      return m_knn_index.at(id).size();
+    }
+    return 0;
+  }
+
+  /// \brief Get the neighbors of the given local point.
+  /// If the point is not stored locally, the function throws an exception.
+  /// \param id Point ID.
+  /// \return The neighbors of the point. A vector of neighbor.
+  std::vector<neighbor_type> get_local_neighbors(const id_type id) const {
+    std::vector<neighbor_type> neighbors;
+    for (auto itr = m_knn_index.neighbors_begin(id),
+              end = m_knn_index.neighbors_end(id);
+         itr != end; ++itr) {
+      neighbors.push_back(*itr);
+    }
+    return neighbors;
+  }
+
+  /// \brief Get the neighbors of the given point.
+  /// This function invokes YGM barrier. All ranks must call this function.
+  template <typename id_iterator>
+  std::unordered_map<id_type, std::vector<neighbor_type>> get_neighbors(
+      id_iterator ids_begin, id_iterator ids_en) const {
+    static_assert(
+        std::is_same_v<typename std::iterator_traits<id_iterator>::value_type,
+                       id_type>,
+        "id_iterator must be an iterator of id_type");
+
+    std::unordered_map<id_type, std::vector<neighbor_type>> neighbors_table;
+    neighbors_table.reserve(std::distance(ids_begin, ids_en));
+
+    static auto& ref_neighbors_table = neighbors_table;
+    ref_neighbors_table              = neighbors_table;
+    m_comm.cf_barrier();
+
+    for (auto it = ids_begin; it != ids_en; ++it) {
+      const auto id = *it;
+      m_comm.async(
+          get_owner(id),
+          [](auto comm, auto pthis, const id_type id, const int source_rank) {
+            assert(pthis->contains_local(id));
+            const auto neighbors = pthis->get_local_neighbors(id);
+            comm->async(
+                source_rank,
+                [](auto, const auto& id, const auto& neighbors) {
+                  ref_neighbors_table.emplace(id, neighbors);
+                },
+                id, neighbors);
+          },
+          m_this, id, m_comm.rank());
+    }
+    m_comm.barrier();
+
+    return neighbors_table;
   }
 
  private:
